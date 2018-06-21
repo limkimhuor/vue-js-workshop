3f9fed51f15400a66f32d57c979e79963b785a61
limkimhuor <kimhuorlim@gmail.com>
2018-06-21 16:05:59 +0700

    Update product

--- exercise-files/update-products/src/components/ManageProducts.vue
+++ exercise-files/update-products/src/components/ManageProducts.vue
@@ -5,7 +5,8 @@
       v-on:submit="onFormSave"
     ></save-product-form>
     <product-list
-      :products="products">
+      :products="products"
+      v-on:edit="onEditClicked">
     </product-list>
   </section>
 </template>
@@ -55,15 +56,28 @@ export default {
   data: initialData,
   methods: {
     onFormSave (product) {
-      // Generate an id using the third-party lib 'uuid'
-      product.id = uuid.v4()
-      // add it to the product list
-      this.products.push(product)
-      // reset the form
+      const index = this.products.findIndex((p) => p.id === product.id)
+      // update product if it exists or create it if it doesn't
+      if (index !== -1) {
+        // We need to replace the array entirely so that vue can recognize
+        // the change and re-render entirely.
+        // See http://vuejs.org/guide/list.html#Caveats
+        this.products.splice(index, 1, product)
+      } else {
+        product.id = uuid.v4()
+        this.products.push(product)
+      }
+
       this.resetProductInForm()
     },
     resetProductInForm () {
       this.productInForm = initialData().productInForm
+    },
+    onEditClicked (product) {
+      // since objects are passed by reference we need to clone the product
+      // either by using Object.assign({}, product) or by using object
+      // spread like we do here
+      this.productInForm = { ...product }
     }
   }
 }
